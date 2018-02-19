a1d3dfdc8e55e1382faf2093289f9b99a0b0d064
Hinko Kocevar <hinkocevar@gmail.com>
2018-02-19 20:35:33 +0100

    WIP

--- app.py
+++ app.py
@@ -16,60 +16,37 @@ def signUp():
 @app.route('/test1/<input1>/<input2>', methods=['GET'])
 def test1(input1, input2):
 #    print("Called test1()\n")
-#    print("input1:", input1)
-#    print("input2:", input2)
+    print("input1:", input1)
+    print("input2:", input2)
 
     datas = []
-#     cons_con = sqlite3.connect('test.db')
     cons_cur = dbcon.cursor()
-#     print("test1(): We have:", q.qsize())
-    try:
-        while True:
-#            datas.append(q.get(False))
-            cons_cur.execute("SELECT rowid,* FROM Frames ORDER BY rowid DESC LIMIT 10")
-            rows = cons_cur.fetchall()
-            for row in rows:
-                datas.append(row)
-            break
-        
-    except queue.Empty:
-#         data = {'type': 'INVALID', 'stamp': str(datetime.datetime.now())}
-        print("test1(): Getting %d frames" % len(datas))
+    id = cons_cur.lastrowid
 
+    print("test1(): We have:", id)
+    cons_cur.execute("SELECT rowid,* FROM Frames ORDER BY rowid DESC LIMIT 10")
+    rows = cons_cur.fetchall()
+    for row in rows:
+        datas.append(row)
+        
     return jsonify(datas)
 
-def producer(q):
-    datas = []
-    
-#     prod_con = sqlite3.connect('test.db')
+def producer():
     prod_cur = dbcon.cursor()
 
     while True:
         print(str(datetime.datetime.now()), ": producing more data ..")
-#        data = {'type': 'BPM', 'stamp': str(datetime.datetime.now())}
-#        q.put(data)
         data = "INSERT INTO Frames VALUES('%s','%s')" % ('BPM', str(datetime.datetime.now()))
         prod_cur.execute(data)
         dbcon.commit()
-#        print("producer(): We have generated:", q.qsize(), "::", data)
-        print("producer(): We have generated:", data)
+        id = prod_cur.lastrowid
+        print("producer(): We have generated:", data, 'with ROW ID: ', id)
         
         time.sleep(1.3)
-    
-#     prod_con.close()
 
 if __name__=="__main__":
     dbcon = sqlite3.connect('test.db')
-#     cur = con.cursor()    
-#     cur.execute('SELECT SQLITE_VERSION()')
-#     data = cur.fetchone()
-#     print("SQLite version: %s" % data)
-#     del con
-#     del cur
-#     del data
-    
-    q = Queue()
-    p = Process(target=producer, args=(q,))
+    p = Process(target=producer)
     p.start()  
     # start web server and listen on all interfaces
     app.run(host='0.0.0.0', debug=True, use_reloader=False)
